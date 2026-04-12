faa2b3c5589b634d491ac47f5e363f234c6d5830
mkacperczyk <255619@edu.p.lodz.pl>
2026-04-12 22:35:34 +0200

    17 dodanie koemtarzy

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,23 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa pilnuje by nie kłaść większych elementów na mniejszych
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    /**
+     * metoda mówi ile liczb zostało już odrzuconych
+     * @return - liczba odrzuconych elementów
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * metoda sprawdza czy stos nie jest pusty i nowa liczba większa od poprzedniej
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
