128b0dc1ab3e822daf061fe52a4b302ada33ade5
mkacperczyk <255619@edu.p.lodz.pl>
2026-04-12 21:32:29 +0200

    14 usuniecie klasy node i przeniesienie jej do intlinkedlist

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -39,3 +39,33 @@ public class IntLinkedList {
     }
 
 }
+class Node {
+
+    private final int value;
+    private Node prev;
+    private Node next;
+
+    public Node(int i) {
+        value = i;
+    }
+
+    int getValue() {
+        return value;
+    }
+
+    Node getNext() {
+        return next;
+    }
+
+    void setNext(Node next) {
+        this.next = next;
+    }
+
+    Node getPrev() {
+        return prev;
+    }
+
+    void setPrev(Node prev) {
+        this.prev = prev;
+    }
+}
